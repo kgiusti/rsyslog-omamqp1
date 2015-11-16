1acb7a38182d2225a07da9a5ef43775466ebf3c4
Kenneth Giusti <kgiusti@redhat.com>
2015-11-16 09:51:23 -0500

    Sends messages, cannot shutdown properly

--- external/python/omamqp1.py
+++ external/python/omamqp1.py
@@ -40,11 +40,15 @@ from proton.reactor import Container, ApplicationEvent, EventInjector
 
 
 class MessageBusHandler(MessagingHandler):
+    """Runs the Proton Message Handler in a dedicated thread.  Messages to be
+    sent to the message bus are made available to the thread via the in_queue.
+    """
     def __init__(self, url, in_queue, injector):
         super(MessageBusHandler, self).__init__(auto_settle=True)
         self._url = url
         self._in_queue = in_queue
         self._injector = injector
+        self._container = None
         self._running = True
         self._thread = threading.Thread(target=self._msg_loop)
         self._thread.daemon = True
@@ -58,25 +62,34 @@ class MessageBusHandler(MessagingHandler):
         print("EXIT")
 
     def _send(self):
+        # pull messages off the queue and send them
+        print("sending messages")
         while self.sender.credit and not self._in_queue.empty():
             msg = self._in_queue.get(False)
             self.sender.send(msg)
-            print("sent message")
+        print("messages sent")
 
     def on_start(self, event):
         print("ON_START %s" % str(event))
-        self.container = event.container
-        self.container.selectable(self._injector)
-        self.sender = self.container.create_sender(self._url)
+        self._container = event.container
+        self._container.selectable(self._injector)
+        self.sender = self._container.create_sender(self._url, name="rsyslog-client")
 
     def on_messages_available(self, event):
-        # pull then off the queue and send them
+        """Application event indicating messages are ready to be fetched from
+        the queue.
+        """
         self._send()
+
+    def on_shutdown(self, event):
+        """Shutdown the Messaging Bus thread
+        """
+        print("Shutting down Message Bus thread")
+        if self._container:
+            self._container.stop()
 
     def on_sendable(self, event):
-        # gabba gabba hey
         self._send()
-
 
     def on_connection_closed(self, event):
         """
@@ -165,12 +178,13 @@ class MessageBusHandler(MessagingHandler):
 
 
 # configs
-URL=127.0.0.1:5672/amq.topic
+URL="127.0.0.1:5672/KEN"
 maxAtOnce=1024
 pollPeriod=0.75
 
 msg_queue = Queue.Queue()
-msg_event = ApplicationEvent("messages_available")
+event_msgs = ApplicationEvent("messages_available")
+event_die = ApplicationEvent("shutdown")
 event_injector = EventInjector()
 
 handler = MessageBusHandler(URL, msg_queue, event_injector)
@@ -179,16 +193,26 @@ def onInit():
     pass
 
 def onExit():
-    pass
+    global event_injector
+    global event_die
+
+    event_injector.trigger(event_die)
+    handler._thread.join(timeout=30)
+    if handler._thread.isAlive():
+        # just warn - we're shutting down anyhow
+        print("Message Bus thread failed to shutdown")
 
 def onReceive(msgs):
+    """Put a list of strings (JSON log output) into a message and hand it over
+    to the Message Bus thread.
+    """
     global msg_queue
-    global msg_event
+    global event_msgs
     global event_injector
 
     pmsg = Message(body=msgs)
     msg_queue.put(pmsg)
-    event_injector.trigger(msg_event)
+    event_injector.trigger(event_msgs)
 
 
 
