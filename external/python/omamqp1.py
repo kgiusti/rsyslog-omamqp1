f7f74da5876e705455d48f73a4f9e8d3d39306cb
Kenneth Giusti <kgiusti@redhat.com>
2015-11-19 15:43:01 -0500

    fix error handling code, pep8 cleanups

--- external/python/omamqp1.py
+++ external/python/omamqp1.py
@@ -24,12 +24,10 @@
 
 """
 
-import sys
-
-# HACK for now:
-sys.path.append("/home/kgiusti/work/rsyslog-omamqp1/external/python/PY27/lib64/python2.7/site-packages")
-
+import logging
+import optparse
 import select
+import sys
 import threading
 try:
     import Queue
@@ -38,23 +36,30 @@ except:
 
 # This plugin relies on the QPID Proton python client API
 # Available at https://pypi.python.org/pypi/python-qpid-proton
-from proton import Message
+from proton import Message, LinkException, SessionException, Url
+from proton import ConnectionException, TransportException
 from proton.handlers import MessagingHandler
 from proton.reactor import Container, ApplicationEvent, EventInjector
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class MessageBusHandler(MessagingHandler):
     """Runs the Proton Message Handler in a dedicated thread.  Messages to be
     sent to the message bus are made available to the thread via the in_queue.
     """
-    def __init__(self, url, in_queue, injector):
+    def __init__(self, urls, target, in_queue, injector, **conn_args):
         super(MessageBusHandler, self).__init__(auto_settle=True)
-        self._url = url
+        self.exception = None
+        self._urls = urls
+        self._connection_args = conn_args
+        self._target = target
         self._in_queue = in_queue
         self._injector = injector
         self._container = None
         self._running = True
         self._thread = threading.Thread(target=self._msg_loop)
+        self._thread.name = "Message Bus I/O Thread"
         self._thread.daemon = True
         self._thread.start()
 
@@ -68,23 +73,26 @@ class MessageBusHandler(MessagingHandler):
                     break
             container.stop()
         except Exception as e:
-            # print("EXCEPT? %s" % str(e))
-            raise
-        #print("EXIT")
+            self.exception = e
+            raise e
+
+    def _connect(self):
+        self._connection_args['urls'] = self._urls
+        self._connection = self._container.connect(**self._connection_args)
+        self._sender = self._container.create_sender(self._connection,
+                                                     target=self._target,
+                                                     name="rsyslog-omamqp1")
 
     def _send(self):
         # pull messages off the queue and send them
-        #print("sending messages")
-        while self.sender.credit and not self._in_queue.empty():
+        while self._sender.credit and not self._in_queue.empty():
             msg = self._in_queue.get(False)
-            self.sender.send(msg)
-        #print("messages sent")
+            self._sender.send(msg)
 
     def on_start(self, event):
-        #print("ON_START %s" % str(event))
         self._container = event.container
         self._container.selectable(self._injector)
-        self.sender = self._container.create_sender(self._url, name="rsyslog-client")
+        self._connect()
 
     def on_messages_available(self, event):
         """Application event indicating messages are ready to be fetched from
@@ -95,73 +103,87 @@ class MessageBusHandler(MessagingHandler):
     def on_shutdown(self, event):
         """Shutdown the Messaging Bus thread
         """
-        #print("Shutting down Message Bus thread")
         self._running = False
 
     def on_sendable(self, event):
         self._send()
 
+    def on_connection_local_close(self, event):
+        pass
+
+    def on_transport_error(self, event):
+        super(MessageBusHandler, self).on_transport_error(event)
+        tport = event.transport
+        if tport.condition and tport.condition.name in self.fatal_conditions:
+            raise TransportException("error: %s" %
+                                     str(event.transport.condition))
+
+    def on_connection_error(self, event):
+        super(MessageBusHandler, self).on_connection_error(event)
+        raise ConnectionException("error: %s" %
+                                  str(event.connection.remote_condition))
+
+    def on_session_error(self, event):
+        super(MessageBusHandler, self).on_session_error(event)
+        raise SessionException("error: %s" %
+                               str(event.session.remote_condition))
+
+    def on_link_error(self, event):
+        super(MessageBusHandler, self).on_link_error(event)
+        raise LinkException("error: %s" % str(event.link.remote_condition))
+
     def on_connection_closed(self, event):
         """
         Called when the connection is closed.
         """
-        #print("on_connection_closed %s" % event)
         pass
 
     def on_session_closed(self, event):
         """
         Called when the session is closed.
         """
-        #print("on_session_closed %s" % event)
         pass
 
     def on_link_closed(self, event):
         """
         Called when the link is closed.
         """
-        #print("    def on_link_closed(self, event): %s" % event)
         pass
 
     def on_connection_closing(self, event):
         """
         Called when the peer initiates the closing of the connection.
         """
-        #print("    def on_connection_closing(self, event): %s" % event)
         pass
 
     def on_session_closing(self, event):
         """
         Called when the peer initiates the closing of the session.
         """
-        #print("    def on_session_closing(self, event): %s" % event)
         pass
 
     def on_link_closing(self, event):
         """
         Called when the peer initiates the closing of the link.
         """
-        #print("    def on_link_closing(self, event): %s" % event)
         pass
 
     def on_disconnected(self, event):
         """
         Called when the socket is disconnected.
         """
-        #print("DISCONNECTED FROM THE MESSAGE BUS - retrying...")
         pass
 
     def on_accepted(self, event):
         """
         Called when the remote peer accepts an outgoing message.
         """
-        #print("    def on_accepted(self, event): %s" % event)
         pass
 
     def on_rejected(self, event):
         """
         Called when the remote peer rejects an outgoing message.
         """
-        #print("    def on_rejected(self, event): %s" % event)
         pass
 
     def on_released(self, event):
@@ -170,7 +192,6 @@ class MessageBusHandler(MessagingHandler):
         that this may be in response to either the RELEASE or MODIFIED
         state as defined by the AMQP specification.
         """
-        #print("    def on_released(self, event): %s" % event)
         pass
 
     def on_settled(self, event):
@@ -179,7 +200,6 @@ class MessageBusHandler(MessagingHandler):
         message. This is the point at which it shouod never be
         retransmitted.
         """
-        #print("    def on_settled(self, event): %s" % event)
         pass
 
     def on_message(self, event):
@@ -190,48 +210,103 @@ class MessageBusHandler(MessagingHandler):
         explicitly accepting it, the ``delivery`` should be used, also
         obtainable via a property on the event.
         """
-        #print("    def on_message(self, event): %s" % event)
         pass
 
     def on_unhandled(self, name, event):
-        #print("UNHANDLED EVENT %s - %s" % (name, str(event)))
         pass
 
     def on_connection_opened(self, event):
-        #print("Connection UP!")
         pass
 
     def on_link_opened(self, event):
-        #print("Link UP!")
         pass
 
 
+def _get_password(password_file):
+    if not password_file:
+        raise ValueError("password_file not provided")
+    if password_file.startswith('pass:'):
+        return password_file[5:]
+    with open(password_file) as f:
+        return f.read().strip(' \n\r')
 
-# configs
-URL="127.0.0.1:5672/KEN"
-maxAtOnce=1024
-pollPeriod=0.75
 
 msg_queue = Queue.Queue()
 event_msgs = ApplicationEvent("messages_available")
 event_die = ApplicationEvent("shutdown")
 event_injector = EventInjector()
+handler = None
 
-handler = MessageBusHandler(URL, msg_queue, event_injector)
 
 def onInit():
-    pass
+    global msg_queue
+    global event_injector
+    global handler
+
+    target = 'rsyslogd'
+    parser = optparse.OptionParser(usage="usage: %prog [options] TARGET",
+                                   description="Send rsyslog messages to"
+                                   " TARGET (default %s) via an AMQP1.0"
+                                   " message bus." % target)
+    parser.add_option("-u", "--url", default="amqp://localhost:5672",
+                      help="address of message bus (default %default)."
+                      " Also accepts a comma-separated list of addresses"
+                      " which will be used for failover.")
+    parser.add_option("--heartbeat", type="int", default=0,
+                      help="A value in seconds indicating the desired"
+                      " frequency of heartbeats used to test the underlying"
+                      " socket is alive.")
+    parser.add_option("--sasl-mechanisms",
+                      help="A space-separated string of SASL mechanisms that"
+                      " are allowed (whitelist). If not specified, the"
+                      " defaults provided by the system are used.")
+    parser.add_option("--username",
+                      help="The username for SASL authentication (if"
+                      " required).")
+    parser.add_option("--password-file",
+                      help="The path to a text file containing the password"
+                      " used for SASL authentication (if required).  If the"
+                      " value starts with the prefix 'pass:' then the"
+                      " remaining text is used as the actual password. This"
+                      " alternative is only meant to be used when security is"
+                      " not necessary (eg. during development) as it will"
+                      " allow the password to be visible to all users of the"
+                      " system.")
+
+    # @TODO(kgiusti) - TBD:
+    # ssl-ca-file
+    # ssl-cert-file
+    # ssl-key-file
+    # ssl-password-file
+    # sasl-disable
+
+    opts, args = parser.parse_args()
+    urls = [u.strip(' ') for u in opts.url.split(',')]
+    if opts.username:
+        password = _get_password(opts.password_file)
+        urls = [str(Url(u, username=opts.username, password=password))
+                for u in urls]
+
+    conn_args = {}
+    if opts.heartbeat:
+        conn_args['heartbeat'] = opts.heartbeat
+    if opts.sasl_mechanisms:
+        conn_args['allowed_mechs'] = opts.sasl_mechanisms.upper()
+    handler = MessageBusHandler(urls, args[0] if args else target,
+                                msg_queue, event_injector, **conn_args)
+
 
 def onExit():
     global event_injector
     global event_die
+    global handler
 
     event_injector.trigger(event_die)
     handler._thread.join(timeout=30)
     if handler._thread.isAlive():
-        # just warn - we're shutting down anyhow
-        #print("Message Bus thread failed to shutdown")
+        # @TODO(kgiusti) just warn - we're shutting down anyhow
         pass
+
 
 def onReceive(msgs):
     """Put a list of strings (JSON log output) into a message and hand it over
@@ -240,30 +315,43 @@ def onReceive(msgs):
     global msg_queue
     global event_msgs
     global event_injector
+    global handler
+
+    # watchdog check to see if I/O thread is ok:
+    if not handler._thread.isAlive():
+        if handler.exception:
+            raise handler.exception
+        raise RuntimeError("message bus I/O thread unexpectedly died")
 
     pmsg = Message(body=msgs)
     msg_queue.put(pmsg)
     event_injector.trigger(event_msgs)
 
 
-
 # main stdin loop, courtesy rsyslog:
+
+maxAtOnce = 1024
+pollPeriod = 0.75
+
 onInit()
 keepRunning = 1
 while keepRunning == 1:
-	while keepRunning and sys.stdin in select.select([sys.stdin], [], [], pollPeriod)[0]:
-		msgs = []
-	        msgsInBatch = 0
-		while keepRunning and sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-			line = sys.stdin.readline()
-			if line:
-				msgs.append(line)
-			else: # an empty line means stdin has been closed
-				keepRunning = 0
-			msgsInBatch = msgsInBatch + 1
-			if msgsInBatch >= maxAtOnce:
-				break;
-		if len(msgs) > 0:
-			onReceive(msgs)
-			sys.stdout.flush() # very important, Python buffers far too much!
+    while keepRunning and sys.stdin in select.select([sys.stdin], [], [],
+                                                     pollPeriod)[0]:
+        msgs = []
+        msgsInBatch = 0
+        while keepRunning and sys.stdin in select.select([sys.stdin], [], [],
+                                                         0)[0]:
+            line = sys.stdin.readline()
+            if line:
+                msgs.append(line)
+                msgsInBatch = msgsInBatch + 1
+            else:
+                # an empty line means stdin has been closed
+                keepRunning = 0
+            if msgsInBatch >= maxAtOnce:
+                break
+        if len(msgs) > 0:
+            onReceive(msgs)
+            sys.stdout.flush()  # very important, Python buffers far too much!
 onExit()
